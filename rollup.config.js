385cb724de1c882dcd3ac3f2f2e0b442b1db3927
Radek Novak <radek.novak.cze@gmail.com>
2020-02-15 20:42:01 +0100

    esm module build

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,46 @@
-import {terser} from "rollup-plugin-terser";
+import { terser } from "rollup-plugin-terser";
+import resolve from "@rollup/plugin-node-resolve";
+import babel from "rollup-plugin-babel";
 import * as meta from "./package.json";
 
 const config = {
   input: "src/index.js",
-  external: Object.keys(meta.dependencies || {}).filter(key => /^d3-/.test(key)),
+  external: Object.keys(meta.dependencies || {}).filter(key =>
+    /^d3-/.test(key)
+  ),
   output: {
     file: `dist/${meta.name}.js`,
     name: "d3",
     format: "umd",
     indent: false,
     extend: true,
-    banner: `// ${meta.homepage} v${meta.version} Copyright ${(new Date).getFullYear()} ${meta.author.name}`,
-    globals: Object.assign({}, ...Object.keys(meta.dependencies || {}).filter(key => /^d3-/.test(key)).map(key => ({[key]: "d3"})))
+    banner: `// ${meta.homepage} v${
+      meta.version
+    } Copyright ${new Date().getFullYear()} ${meta.author.name}`,
+    globals: Object.assign(
+      {},
+      ...Object.keys(meta.dependencies || {})
+        .filter(key => /^d3-/.test(key))
+        .map(key => ({ [key]: "d3" }))
+    )
   },
   plugins: []
+};
+
+const moduleConfig = {
+  ...config,
+  output: {
+    name: config.name,
+    extend: config.extend,
+    banner: config.banner,
+    globals: config.globals
+  },
+  plugins: [
+    resolve(),
+    babel({
+      exclude: "node_modules/**"
+    })
+  ]
 };
 
 export default [
@@ -32,5 +59,17 @@ export default [
         }
       })
     ]
+  },
+  {
+    ...moduleConfig,
+    ...{
+      output: { format: "esm", dir: "dist/esm" }
+    }
+  },
+  {
+    ...moduleConfig,
+    ...{
+      output: { format: "cjs", dir: "dist/cjs" }
+    }
   }
 ];
